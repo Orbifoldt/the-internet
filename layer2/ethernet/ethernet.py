b76a5ae6d99b8cb0fc04096ebf69cb059d2f2a33
Orbifoldt <eikenrode02@gmail.com>
2021-10-23 12:11:52 +0200

    Refactoring the two ethernet frame standards

--- layer2/ethernet/ethernet.py
+++ layer2/ethernet/ethernet.py
@@ -1,22 +1,12 @@
 from enum import Enum
-from typing import Final
+from typing import Final, final
 
 from bitstring import BitArray
 from layer2.mac import Mac
 from layer2.tools import crc32
 
 
-class EtherType(Enum):
-    IPV4 = 0x0800
-    IPV6 = 0x86DD
-    ARP = 0x0806
-
-    def to_bytes(self):
-        return self.value.to_bytes(2, byteorder="big")
-
-
-# TODO: refactor to extract common functionality from this and the 802.3 frame
-class EthernetFrame:
+class EthernetFrameBase:
     preamble: Final = int("10" * 28, 2).to_bytes(7, byteorder="big")
     start_frame_delim: Final = int("10101011", 2).to_bytes(1, byteorder="big")
     inter_packet_gap_size: Final = 96
@@ -24,9 +14,7 @@ class EthernetFrame:
     MIN_PAYLOAD = 46
     MAX_PAYLOAD = 1500
 
-    def __init__(self, destination: Mac, source: Mac, payload: bytes, ether_type: EtherType = EtherType.IPV4) -> None:
-        if ether_type != EtherType.IPV4:
-            raise ValueError("This ether type is not yet implemented")
+    def __init__(self, destination: Mac, source: Mac, payload: bytes, headers: bytes) -> None:
         if len(payload) > self.MAX_PAYLOAD:
             raise ValueError(f"Max payload size si {self.MAX_PAYLOAD} bytes, received {len(payload)} bytes.")
         if len(payload) < self.MIN_PAYLOAD:
@@ -34,16 +22,16 @@ class EthernetFrame:
 
         self.destination = destination
         self.source = source
-        self.ether_type = ether_type
+        self.other_headers = headers
         self.payload = payload
         self.fcs = self.calculate_fcs()
 
     def calculate_fcs(self):
-        all_data = self.destination.address + self.source.address + self.ether_type.to_bytes() + self.payload
+        all_data = self.destination.address + self.source.address + self.other_headers + self.payload
         return crc32(all_data)
 
     def bytes(self):
-        return self.destination.address + self.source.address + self.ether_type.to_bytes() + self.payload + self.fcs
+        return self.destination.address + self.source.address + self.other_headers + self.payload + self.fcs
 
     def phys_bytes(self):
         return self.preamble + self.start_frame_delim + self.bytes()
@@ -56,6 +44,29 @@ class EthernetFrame:
 
     def __eq__(self, other):
         return isinstance(other, self.__class__) and other.bytes() == self.bytes()
+
+    def fcs_is_valid(self):
+        return self.fcs == self.calculate_fcs()
+
+
+class EtherType(Enum):
+    IPV4 = 0x0800
+    IPV6 = 0x86DD
+    ARP = 0x0806
+
+    def to_bytes(self):
+        return self.value.to_bytes(2, byteorder="big")
+
+
+class EthernetFrame(EthernetFrameBase):
+    MIN_PAYLOAD: Final = 46
+    MAX_PAYLOAD: Final = 1500
+
+    def __init__(self, destination: Mac, source: Mac, payload: bytes, ether_type: EtherType = EtherType.IPV4) -> None:
+        if ether_type != EtherType.IPV4:
+            raise ValueError("This ether type is not yet implemented")
+        super().__init__(destination, source, payload, ether_type.to_bytes())
+        self.ether_type = ether_type
 
 
 class LlcType(Enum):  # IEEE 802.1Q
@@ -71,47 +82,17 @@ class LlcType(Enum):  # IEEE 802.1Q
         return self.to_bytes()[-2:]
 
 
-class Ethernet802_3Frame:
-    preamble: Final = int("10" * 28, 2).to_bytes(7, byteorder="big")
-    start_frame_delim: Final = int("10101011", 2).to_bytes(1, byteorder="big")
-    inter_packet_gap_size: Final = 96
-
+class Ethernet802_3Frame(EthernetFrameBase):
     MIN_PAYLOAD: Final = 42
     MAX_PAYLOAD: Final = 1500
 
     def __init__(self, destination: Mac, source: Mac, payload: bytes, llc_type: LlcType = LlcType.DEFAULT) -> None:
         if llc_type != LlcType.DEFAULT:
             raise ValueError("This LLC type is not yet implemented")
-        if len(payload) > self.MAX_PAYLOAD:
-            raise ValueError(f"Max payload size si {self.MAX_PAYLOAD} bytes, received {len(payload)} bytes.")
 
         self.size = len(payload).to_bytes(2, byteorder='big')
         if len(payload) < self.MIN_PAYLOAD:
             payload += bytearray(self.MIN_PAYLOAD - len(payload))
 
-        self.destination = destination
-        self.source = source
+        super().__init__(destination, source, payload, self.size + llc_type.to_bytes())
         self.llc_type = llc_type
-        self.payload = payload
-        self.fcs = self.calculate_fcs()
-
-    def calculate_fcs(self):
-        all_data = self.destination.address + self.source.address + self.size \
-                   + self.llc_type.to_bytes() + self.payload
-        return crc32(all_data)
-
-    def bytes(self):
-        return self.destination.address + self.source.address + self.size + self.llc_type.to_bytes() + self.payload \
-               + self.fcs
-
-    def phys_bytes(self):
-        return self.preamble + self.start_frame_delim + self.bytes()
-
-    def bits(self):
-        return BitArray(auto=self.bytes())
-
-    def phys_bits(self):
-        return BitArray(auto=self.phys_bytes())
-
-    def __eq__(self, other):
-        return isinstance(other, self.__class__) and other.bytes() == self.bytes()
